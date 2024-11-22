53d8e6e8f3ac3df440b4e3f70ed3560d44541b9f
hoangvanthang159 <hoangthang15t9@gmail.com>
2024-11-22 16:05:33 +0700

    Delete intro-159/intro-159 directory

--- intro-159/intro-159/src/Data.java
+++
@@ -1,7 +0,0 @@
-
-public class Data {
-    private static final String FILEKHACHHANG = "File-Khachhang";
-    private static final String FILENGANHANG = "File-Nganhang";
-    private static final String FILESOTIETKIEM = "C:UsersadminDownloadsSteamSetup (2).exe";
-
-}
--- intro-159/intro-159/src/Exceptionhandling.java
+++
@@ -1,5 +0,0 @@
-public class Exceptionhandling extends Exception{
-    public Exceptionhandling(String message) {
-        super(message);
-    }
-}
